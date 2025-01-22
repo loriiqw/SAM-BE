17518a61d677bea5bd24d0263bd294ddebaf287a
loriiqw <lorainedeleonn@gmail.com>
2025-01-22 13:16:25 +0800

    clean connection.php

--- F01/connection.php
+++ F01/connection.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost";  // Database server
-$username = "root";         // Database username
-$password = "";             // Database password (default is empty for local environments)
-$dbname = "olympics";  // Database name
+$servername = "localhost"; 
+$username = "root";         
+$password = "";           
+$dbname = "olympics"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
